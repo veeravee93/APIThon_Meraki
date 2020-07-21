cdd14ebdb83dd28ef403ba9eba9684432bdf5aca
Veerabathiran R <veeravee93@gmail.com>
2020-07-21 11:54:12 +0530

    whole jar is not getting uploaded due to size.
    openbanking-java-sdk\core\src\main\java\com\bankofapis\core\model\accounts

---
+++ OBReadTransaction.java
@@ -0,0 +1,108 @@
+package com.bankofapis.core.model.accounts;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+
+public class OBReadTransaction {
+
+    @JsonProperty("AccountId")
+    private String accountId;
+
+    @JsonProperty("CreditDebitIndicator")
+    private String creditDebitIndicator;
+
+    @JsonProperty("Status")
+    private String status;
+
+    @JsonProperty("BookingDateTime")
+    private String bookingDateTime;
+
+    @JsonProperty("Amount")
+    private OBReadAmount amount;
+
+    @JsonProperty("ProprietaryBankTransactionCode")
+    private OBReadProprietaryBankTransactionCode proprietaryBankTransactionCode;
+
+    @JsonProperty("TransactionInformation")
+    private String transactionInformation;
+
+    @JsonProperty("Balance")
+    private OBReadBalance balance;
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getCreditDebitIndicator() {
+    	
+    	if(creditDebitIndicator.equals("Debit")) {
+    		return creditDebitIndicator;
+    	}
+    	else
+    		return ("Not a Debit");
+    }
+
+    public void setCreditDebitIndicator(String creditDebitIndicator) {
+        this.creditDebitIndicator = creditDebitIndicator;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getBookingDateTime() {
+        return bookingDateTime;
+    }
+
+    public void setBookingDateTime(String bookingDateTime) {
+        this.bookingDateTime = bookingDateTime;
+    }
+
+    public OBReadAmount getAmount() {
+        return amount;
+    }
+
+    public void setAmount(OBReadAmount amount) {
+        this.amount = amount;
+    }
+
+    public OBReadProprietaryBankTransactionCode getProprietaryBankTransactionCode() {
+        return proprietaryBankTransactionCode;
+    }
+
+    public void setProprietaryBankTransactionCode(OBReadProprietaryBankTransactionCode proprietaryBankTransactionCode) {
+        this.proprietaryBankTransactionCode = proprietaryBankTransactionCode;
+    }
+
+    public String getTransactionInformation() {
+    	if(creditDebitIndicator.equals("Debit")) {
+    		return ("Interest Deduction");
+    	}
+    	else
+    		return transactionInformation;
+    }
+
+    public void setTransactionInformation(String transactionInformation) {
+        this.transactionInformation = transactionInformation;
+    }
+
+    public OBReadBalance getBalance() {
+        return balance;
+    }
+
+    @JsonProperty("Balance")
+    public void setBalance(OBReadBalance balance) {
+        this.balance = balance;
+    }
+
+}
