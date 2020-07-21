d96356d7c4defa5a7f6ee4090ffdcc072dc92ff0
Veerabathiran R <veeravee93@gmail.com>
2020-07-21 11:56:13 +0530

    whole package is too big to upload
    openbanking-ui\packages\web-app\src

---
+++ Dashboard.js
@@ -0,0 +1,62 @@
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { initializeJourney } from '@openbanking/ui-data/lib/services/auth-service'
+import { setAccessToken } from '@openbanking/ui-data/lib/actions/auth'
+import { setAccountId } from '@openbanking/ui-data/src/actions/account'
+
+const Dashboard = () => {
+    const dispatch = useDispatch()
+
+    useEffect(()=>{
+        //remove access_token to start journey again
+        localStorage.removeItem('token')
+        localStorage.removeItem('refresh_token')
+        dispatch(setAccessToken(null,null))
+        dispatch(setAccountId(null))
+    },[])
+
+    function setType(type) {
+        // initialize aisp/pisp journey to get authorization URL
+        initializeJourney(dispatch, type)
+    }
+
+    return (
+        <div className="dashboard">
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm">
+                        <div className="text">
+                            <h3>Open Banking</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm">
+                        <div className="journeyBtns">
+                            <button
+                                className="journeyBtn"
+                                onClick={() => setType('aisp')}
+                            >
+                                Account Information Service Provider (AISP)
+                            </button>
+                            <button
+                                className="journeyBtn"
+                                // to do 
+                            >
+                                Payment Initiation Service Provider (PISP)
+                            </button>
+							<button
+                                className="journeyBtn"
+                                // to do 
+                            >
+                                Welcome to Financial Plans (Savings/Insurance plan details)
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Dashboard
